657ea6e6562611ce118b5184e31989a1c160db53
DivineProgram <113607171+DivineProgram@users.noreply.github.com>
2023-08-18 08:40:38 +0000

    Grid much more organised and accessible
    The grid option can now be printed out, which will output its traditional format, and slots can now be set, with the bottom left being coordinate (0,0). Also made some changes to the README.

--- README.md
+++ README.md
@@ -5,15 +5,5 @@ The data is stored in a 2D list, with each list representing a column and then e
 The grid will automatically scale to the contents of the grid, unless a cell width is specified (which might not actually work now I'm thinking about it).
 When entering co-ordinates, a format is followed consistently.
 
-*When accessing the raw data array, access it the following syntax:*
-
-  `textGrid.contents[ X-coordinate ][ Y-coordinate ]`
-  
-*When using built-in functions/procedures, using the format:*
-
-  `[ X-coordinate , Y-coordinate ]`
-  
-*Use this format as your argument in the procedure or function you'd like to use*
-
-For a list of all usable functions, classes, procedures, attributes, and any syntax you may find useful in the module, use the procedure: `eg.define(obj=None)`.
-This will give you all attributes of the object specified through `obj`. When `obj = None`, the properties, attributes of the module itself are defined in terms of their syntax. No explanation is given to these, when they are being defined.
+**You can find the code in `esperGrid.py`.**
+The file organisation is really messy due to the way that Replit runs things.
--- esperGrid.py
+++ esperGrid.py
@@ -14,9 +14,44 @@ Good fo simple programs that need a simple grid UI
 
 class grid:
   
-  def __init__(self, width=10, height=10, min_length=-1, placeholder=' '):
+  def __init__(self, width=10, height=10, min_length=-1, placeholder=' ', column_gap=1):
     self.width = int(width)
     self.height = int(height)
     self.min_length = int(min_length)
     self.placeholder = placeholder
-    self.content = [[None for w in range(self.width)] for h in range(self.height)]
+    self.column_gap = column_gap
+    self.content = [[None for h in range(self.height)] for w in range(self.width)]
+
+  def __str__(self):
+    output = ''
+    max = self.find_max()
+    for y in range(self.height-1, -1, -1):
+      for x in range(self.width):
+        if self.content[x][y] == None:
+          output += '{0:<{w}}'.format(self.placeholder, w=max)
+        else:
+          output += '{0:<{w}}'.format(self.content[x][y], w=max)
+        if x != self.width-1:
+          output += ' '*self.column_gap
+      if y != 0:
+        output += '\n'
+    return output
+
+  def find_max(self):
+    max_length = self.min_length
+    for y in range(self.height):
+      for x in range(self.width):
+        if self.content[x][y] == None:
+          current_length = len(self.placeholder)
+        else:
+          current_length = len(self.content[x][y])
+        if current_length > max_length:
+          max_length = current_length
+    return max_length
+
+  def set(self, new, coord=(0,0)):
+    self.content[coord[0]][coord[1]] = new
+
+g = grid(placeholder='.')
+g.set('#', (3,3))
+print(g)
