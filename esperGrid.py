69119059a82346d6a698ec87dad8d336246fa7c4
DivineProgram <113607171+DivineProgram@users.noreply.github.com>
2023-08-18 11:14:24 +0000

    Added new methods, functions and comments
    Added new set methods, one new function, started writing an unfinished dunder method, and labelled the code with comments

--- esperGrid.py
+++ esperGrid.py
@@ -12,7 +12,9 @@ Good fo simple programs that need a simple grid UI
 
 ############### GENERAL ###############
 
-class grid:
+class textgrid:
+
+  ## DUNDER ##
   
   def __init__(self, width=10, height=10, min_length=-1, placeholder=' ', column_gap=1):
     self.width = int(width)
@@ -37,7 +39,20 @@ class grid:
         output += '\n'
     return output
 
-  def find_max(self):
+    def __get__(self):
+      pass
+
+  ## FUNCTIONS ##
+
+  def index(self, *args: str) -> list:
+    output = []
+    for y in range(self.height):
+      for x in range(self.width):
+        if self.content[x][y] in args:
+          output.append((x,y))
+    return output
+
+  def find_max(self) -> int:
     max_length = self.min_length
     for y in range(self.height):
       for x in range(self.width):
@@ -49,9 +64,22 @@ class grid:
           max_length = current_length
     return max_length
 
-  def set(self, new, coord=(0,0)):
-    self.content[coord[0]][coord[1]] = new
+  ## METHODS ##
 
-g = grid(placeholder='.')
-g.set('#', (3,3))
-print(g)
+  def set(self, item, coord=(0,0)):
+    item = str(item)
+    self.content[coord[0]][coord[1]] = item
+
+  def set_many(self, item, coords=[(0,0)]):
+    for coord in coords:
+      self.set(item, coord)
+
+  def spread(self, item, quantity: int=1):
+    quantity = int(quantity)
+    for i in range(quantity):
+      self.set(item,(rn.randint(0,self.width-1),
+                   rn.randint(0,self.height-1)))
+
+grid = textgrid(placeholder='.')
+grid.spread('#', 3)
+print(grid)
